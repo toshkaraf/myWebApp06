6dfe5a1c881067a5dcf769b7aea46c7ae85f7718
Grigory Kislin <javawebinar@yandex.ru>
2015-10-02 23:18:57 +0300

    String and valueOf

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,6 @@
 package main;
 
-import main.ru.javawebinar.webapp.model.ContactType;
-import main.ru.javawebinar.webapp.model.Link;
-import main.ru.javawebinar.webapp.model.MultiTextSection;
-import main.ru.javawebinar.webapp.model.Resume;
-
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 /**
@@ -13,8 +9,20 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        for (ContactType contact : ContactType.values()) {
-            System.out.println(contact.toHtml("value"));
-        }
+        String value = "value";
+        System.out.println(value.charAt(3));
+        byte[] bytes = value.getBytes();
+        System.out.println(Charset.defaultCharset().name());
+        char[] chars = value.toCharArray();
+        System.out.println(Arrays.toString(bytes));
+        String a = "abc";
+        String b = ("ab" + new String("c")).intern();
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        Integer integer1 = Integer.valueOf(200);
+        Integer integer2 = Integer.valueOf(200);
+        System.out.println(integer1 == integer2);
+        System.out.println(integer1.equals(integer2));
+
     }
 }
--- src/multiinheritance/Main.java
+++ src/multiinheritance/Main.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         IVehicle[] vehicles = new Vehicle[3];
+        ILandVehicle[] vehicles2 = new LandVehicle[5];
+        System.out.println(vehicles.getClass().equals(vehicles2.getClass()));
+
         System.out.println(vehicles.length + " " + vehicles.getClass());
         LandVehicle landVehicle = new LandVehicle(170, 100, 4);
         WaterVehicle waterVehicle = new WaterVehicle(40, 300, 300);
