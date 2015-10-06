c3b5ee45e030aa740506f6c61db2ce6bb6ff733d
Grigory Kislin <javawebinar@yandex.ru>
2015-10-06 22:56:24 +0300

    Add fullName.

--- src/main/ru/javawebinar/webapp/model/Resume.java
+++ src/main/ru/javawebinar/webapp/model/Resume.java
@@ -9,17 +9,23 @@ import java.util.UUID;
  */
 public class Resume {
     private final String uuid;
+    private final String fullName;
     private final Map<ContactType, String> contacts;
     private final Map<SectionType, Section> sections;
 
-    public Resume(Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
-        this(UUID.randomUUID().toString(), contacts, sections);
+    public Resume(String fullName, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
+        this(UUID.randomUUID().toString(), fullName, contacts, sections);
     }
 
-    public Resume(String uuid, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
+    public Resume(String uuid, String fullName, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
         this.uuid = uuid;
+        this.fullName = fullName;
         this.contacts = contacts;
         this.sections = sections;
+    }
+
+    public String getUuid() {
+        return uuid;
     }
 
     public Map<ContactType, String> getContacts() {
