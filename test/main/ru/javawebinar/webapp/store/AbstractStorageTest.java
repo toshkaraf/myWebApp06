af627c98d1983823b33a57905a1da821fa4fecc4
Grigory Kislin <javawebinar@yandex.ru>
2015-10-09 21:59:53 +0300

    Add test mock

---
+++ test/main/ru/javawebinar/webapp/store/AbstractStorageTest.java
@@ -0,0 +1,20 @@
+package main.ru.javawebinar.webapp.store;
+
+import static org.junit.Assert.*;
+
+/**
+ * GKislin
+ * 09.10.2015.
+ */
+public class AbstractStorageTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
