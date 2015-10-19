6313326b7386ea43b6ab70afe589d472a23e50e3
Anton Rafes <rafes@ukr.net>
2015-10-19 17:44:18 +0300

    add AbstractMapStorage

---
+++ src/ru/javawebinar/webapp/storage/AbstractMapStorage.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.webapp.storage;
+
+/**
+ * Created by Антон on 19.10.2015.
+ */
+public abstract class AbstractMapStorage extends AbstractStorage {
+}
--- src/ru/javawebinar/webapp/storage/MapStorage.java
+++ src/ru/javawebinar/webapp/storage/MapStorage.java
@@ -1,9 +1,58 @@
 package ru.javawebinar.webapp.storage;
 
+import ru.javawebinar.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * GKislin
  * 16.10.2015.
  */
-public class MapStorage {
-    // TODO implemet IStorage
+public class MapStorage extends AbstractMapStorage{
+    public Map<String, Resume> storage = new TreeMap<>();
+
+    @Override
+    protected boolean exist(String uuid) {
+        return storage.containsKey(uuid);
+    }
+
+    @Override
+    protected void doClear() {
+        storage.clear();
+    }
+
+    @Override
+    protected void doSave(Resume r) {
+        storage.put(r.getUuid(),r);
+    }
+
+    @Override
+    protected void doUpdate(Resume r) {
+        storage.put(r.getUuid(),r);
+    }
+
+    @Override
+    protected Resume doLoad(String uuid) {
+        storage.get(uuid);
+        return null;
+    }
+
+    @Override
+    protected void doDelete(String uuid) {
+        storage.remove(uuid);
+    }
+
+    @Override
+    protected List<Resume> doGetAll() {
+        return new ArrayList(storage.values());
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
 }
